7d495fcdb620f36e490339938da615d073dc8ee8
baddddddddd <eluxeofficial@gmail.com>
2025-06-06 12:10:02 +0800

    Add CIFAR datasets for training

--- training/datasets/__init__.py
+++ training/datasets/__init__.py
@@ -1,1 +1,4 @@
 from training.datasets.mnist import MNISTTrainer
+from training.datasets.fashion_mnist import FashionMNISTTrainer
+from training.datasets.cifar_10 import CIFAR10Trainer
+from training.datasets.cifar_100 import CIFAR100Trainer
---
+++ training/datasets/fashion_mnist.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+from training.trainer import Trainer
+
+class FashionMNISTTrainer:
+    def __init__(self, model, optimizer, batch_size=64):
+        self.train_dataset = datasets.FashionMNIST(
+            root='./data',
+            train=True,
+            transform=ToTensor(),
+            download=True
+        )
+
+        self.test_dataset = datasets.FashionMNIST(
+            root='./data',
+            train=False,
+            transform=ToTensor(),
+            download=True
+        )
+
+        self.train_dataloader = DataLoader(self.train_dataset, batch_size=batch_size)
+        self.test_dataloader = DataLoader(self.test_dataset, batch_size=batch_size)
+
+        self.model = model
+        self.loss_fn = nn.CrossEntropyLoss()
+        self.optimizer = optimizer
+        self.trainer = Trainer(self.train_dataloader, self.test_dataloader, self.model, self.loss_fn, self.optimizer)
+
+
+    def train(self, epochs):
+        self.trainer.fit(epochs)
